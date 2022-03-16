9dd04ba89889eac27b989ea2f0620cc9d8b0d6ee
khalid abdullah <92260313+khalid1418@users.noreply.github.com>
2022-03-16 21:42:32 +0300

    Revert "- add delete newest"

--- app/src/main/java/com/khalid/daybyday/detailNoteUi/DetailFragment.kt
+++ app/src/main/java/com/khalid/daybyday/detailNoteUi/DetailFragment.kt
@@ -1,30 +1,22 @@
 package com.khalid.daybyday.detailNoteUi
 
-import android.app.AlertDialog
 import android.os.Bundle
-import android.view.*
 import androidx.fragment.app.Fragment
-import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.activityViewModels
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.khalid.daybyday.R
-import com.khalid.daybyday.dataLayer.roomData.NoteDataModel
 import com.khalid.daybyday.databinding.FragmentDetailBinding
 import com.khalid.daybyday.databinding.FragmentShowListNoteBinding
-import com.khalid.daybyday.showListNoteUi.ShowListNoteViewModel
-import com.khalid.daybyday.utils.DayByDayApplication
-import com.khalid.daybyday.utils.ViewModelFactory
 
 
 class DetailFragment : Fragment() {
     private var _binding: FragmentDetailBinding? = null
     private val binding get() = _binding
     private val navArgument: DetailFragmentArgs by navArgs()
-    private val viewModel: ShowListNoteViewModel by activityViewModels {
-        ViewModelFactory((activity?.application as DayByDayApplication).repository)
-    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -32,27 +24,12 @@ class DetailFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentDetailBinding.inflate(inflater, container, false)
-        setHasOptionsMenu(true)
-
         return binding?.root
 
-    }
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(R.menu.update_note_fragment_menu, menu)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     alltask()
-        (activity as AppCompatActivity).supportActionBar?.title = "Details ${navArgument.title}"
-
-
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId){
-            R.id.menu_delete -> confirmItemDelete()
-        }
-        return super.onOptionsItemSelected(item)
     }
 
     fun alltask() {
@@ -67,21 +44,6 @@ class DetailFragment : Fragment() {
         val action = DetailFragmentDirections.actionDetailFragmentToEditNoteFragment(navArgument.id , navArgument.title , navArgument.description)
         findNavController().navigate(action)
     }
-
-    private fun confirmItemDelete() {
-        val builder = AlertDialog.Builder(requireContext())
-        builder.setPositiveButton("Yes"){_,_ ->
-            viewModel.delete(NoteDataModel(navArgument.id,navArgument.title,navArgument.description))
-            Toast.makeText(requireContext(),"Successfully Removed '${navArgument.title}'",
-                Toast.LENGTH_SHORT).show()
-            findNavController().navigateUp()
-        }
-        builder.setNegativeButton("No"){_,_ -> }
-        builder.setTitle("Delete'${navArgument.title}' ?")
-        builder.setMessage("Are you sure you want remove '${navArgument.title}' ?")
-        builder.create().show()
-    }
-
 
     override fun onDestroyView() {
         super.onDestroyView()
